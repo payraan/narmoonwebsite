0f8f01ec53e6d6008720d6be01d075d65aba3102
نام شما < >
2025-06-01 20:52:44 +0300

    🔧 Fix syntax error in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-wfrom fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, Response, RedirectResponse, PlainTextResponse
